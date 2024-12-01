7fb282ee7ed03f6ec92c4e853425629b5f0ea14b
mr-thierry <thierry.roy@aexp.com>
2024-12-01 08:35:45 -0500

    Day01

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,3 +13,9 @@ tasks {
         gradleVersion = "8.11.1"
     }
 }
+
+
+dependencies {
+    implementation("org.junit.jupiter:junit-jupiter:5.9.0")
+    implementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
+}
--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,76 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.Test
+import kotlin.math.abs
+
+
+class Day01 {
+
+    private val day = "Day01"
+
+    @Test
+    fun testSolution1() {
+        assertThat(solution1(readInput("${day}_test"))).isEqualTo(11)
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    @Test
+    fun solution1() {
+        assertThat(solution1(readInput(day))).isEqualTo(1258579)
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    @Test
+    fun testSolution2() {
+        assertThat(solution2(readInput("${day}_test"))).isEqualTo(31)
+    }
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    @Test
+    fun solution2() {
+        assertThat(solution2(readInput(day))).isEqualTo(23981443)
+    }
 
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    private fun solution1(input: List<String>): Int {
+        val listLeft = mutableListOf<Int>()
+        val listRight = mutableListOf<Int>()
+
+        input.forEach { string ->
+            string.split(" ").filter { it.isNotBlank() }.let { numbers ->
+                listLeft.add(numbers[0].toInt())
+                listRight.add(numbers[1].toInt())
+            }
+        }
+        listLeft.sort()
+        listRight.sort()
+
+        var result = 0
+
+        listLeft.forEachIndexed { index, i ->
+            result += abs(listLeft[index] - listRight[index])
+        }
+
+        return result
+
+    }
+
+    private fun solution2(input: List<String>): Int {
+        val listLeft = mutableListOf<Int>()
+        val listRight = mutableListOf<Int>()
+
+        input.forEach { string ->
+            string.split(" ").filter { it.isNotBlank() }.let { numbers ->
+                listLeft.add(numbers[0].toInt())
+                listRight.add(numbers[1].toInt())
+            }
+        }
+
+        var result = 0
+        listLeft.forEach { leftNb ->
+            var count = leftNb * listRight.count { rightNb -> rightNb == leftNb }
+            result += count
+        }
+
+
+        return result
+    }
+
+
 }
